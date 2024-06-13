59d7c8d30d6637fcb2e123a929272f1eb811a049
wettestsock <evilsharkeater@gmail.com>
2024-06-13 13:34:08 -0400

    first commit

---
+++ first.s
@@ -0,0 +1,17 @@
+
+
+; DATA SECTION
+; stores variables, what to store in memory
+section .data 
+
+
+
+; actual code thats being run
+section .text
+global _start 
+
+
+_start: ; LABEL, segment of code, everything below start will run
+; basically int main() {}
+
+
