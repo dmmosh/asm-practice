beaa0087d7a042980f5d491fa190c3de4524421a
wettestsock <evilsharkeater@gmail.com>
2024-06-13 19:50:36 -0400

    nothing of note

--- characters.s
+++ characters.s
@@ -2,6 +2,7 @@
 
 section .data
     character DB 'A' ; 1 byte char
+    arr DB 1,2,3,4 ; array, 
 
 
 section .text
@@ -10,6 +11,6 @@ global _start
 
 
 _start:
-    MOV bl,[character]
     MOV eax,1
+    MOV bl,[arr]
     INT 80h
