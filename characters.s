961da5f05e9824e76b513fd3316fc42ae0e5bae3
wettestsock <evilsharkeater@gmail.com>
2024-06-13 19:55:33 -0400

    nothing of note

--- characters.s
+++ characters.s
@@ -2,7 +2,10 @@
 
 section .data
     character DB 'A' ; 1 byte char
+    character2 DB 'B' ; 1 byte char
     arr DB 1,2,3,4 ; array, 
+    ; arrays have to way to end, so have to manually put something at the end to indicate the end
+    ; null terminator
 
 
 section .text
@@ -11,6 +14,7 @@ global _start
 
 
 _start:
-    MOV bl,[arr]
+    MOV bl,[character]
+    MOV bh,[character2]
     MOV eax,1
     INT 80h
