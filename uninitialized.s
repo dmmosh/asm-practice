d92d407da4de1a8b8904517db18e5d26dc1245d6
wettestsock <evilsharkeater@gmail.com>
2024-06-13 20:15:56 -0400

    nothing of note

--- uninitialized.s
+++ uninitialized.s
@@ -10,13 +10,13 @@ section .bss ; block starting space
 section .text
 global _start
 
-
+; nasm -f elf -o uninitialized.o uninitialized.s && ld -m elf_i386 -o out uninitialized.o && git-all && ./out && echo $? 
 
 _start:
-    MOV bl, 1
-    MOV [num], bl
-    MOV [num+1], bl ; note: boxy brackets 
-    MOV [num+2], bl
+    MOV bl,1
+    MOV [num],bl
+    MOV [num+1],bl ; note: boxy brackets 
+    MOV [num+2],bl
 
-    MOV eax 1
+    MOV eax,1
     INT 80h
