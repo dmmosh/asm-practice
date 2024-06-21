d74d1e1bf441fc29bb3f9b332e9e96b67fd53461
wettestsock <evilsharkeater@gmail.com>
2024-06-21 13:16:47 -0400

    nothing of note

--- add.s
+++ add.s
@@ -9,6 +9,10 @@ section .bss
 section .text
 global _start
 
+; nasm -f elf -o add.o add.s && ld -m elf_i386 -o out add.o && git-all && ./out
+
 
 _start:
-    
+    MOV eax,1
+    MOV ebx,20
+    INT 80h
