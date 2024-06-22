acd2f4e9546f8f67dc0043a5553d2e7f3f5e8147
wettestsock <evilsharkeater@gmail.com>
2024-06-22 13:11:52 -0400

    nothing of note

--- add.s
+++ add.s
@@ -15,17 +15,23 @@ global _start
 
 ; EFLAGS
 ; a register which gives info on whats just been run
+; each bit is a flag representing what just been run
 
 ; PF flag: parody flag , set when value is odd # (1), when even (0) 
 ; basically modulus
+
+; registors vs variables
+; registors: operations are way faster than variables
+; variables
 
 _start:
     MOV eax,5
     MOV [num1],eax
     MOV [num2], eax
     MOV ebx,30
-    ADD [num1],ebx
+    MOV eax,0b00000001
+    ADD [num1],eax
 
-    MOV eax,1
-    MOV ebx,[num1]
-    INT 80h
+    MOV eax,1 ; system reads code 1, meaning exit
+    MOV ebx,[num1] ; system returns the code
+    INT 80h ; requests system call
